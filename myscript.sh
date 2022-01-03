5690317feae4972566bb25191f055813e273a7c8
ecrseer <juustino@live.com>
2022-01-03 13:57:36 -0300

    handler

--- myscript.sh
+++ myscript.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
-
+echo "paste this on /boot/grub/grub.cfg"
+echo "radeon.cik_support=0 amdgpu.cik_support=1 binder.devices=binder,hwbinder,vndbinder,anbox-binder,anbox-hwbinder,anbox-vndbinder psi=1"
 #enable aur no pamac
 sed -Ei '/EnableAUR/s/^#//' /etc/pamac.conf
+cat IgnorePkg   = greetd-tuigreet
 #lxc versao correta
 pacman -U https://archive.archlinux.org/packages/path/lxc-1:4.0.10-2-x86_64.pkg.tar.zst
 
